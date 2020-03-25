bc632e6e694a7a5e24a449cd52ec24faf2600d44
IsisPMR <piorctt@gmail.com>
2020-03-24 19:10:55 -0600

    id, name, image

--- src/Components/search.js
+++ src/Components/search.js
@@ -3,13 +3,14 @@ import "./search.css";
 /* import CatchPokemons from './Components/CatchPokemons'; */
 
 export default class Search extends Component {
-
+    
     fecthPokemon(pokeName){
+        let pokemons = []; 
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
           .then(response => response.json())
           .then(pokemon => {
-            console.log(pokemon)
-            return pokemon
+            console.log(pokemon.id,pokemon.name,pokemon.sprites.front_default)
+            pokemons.push(pokemon)
           })
     }
           
@@ -18,10 +19,8 @@ export default class Search extends Component {
       <div>
         <h2>Welcome!, looking for pokemons? click here: </h2>
         <input label="Search"></input>
-        <button className="catchButton" onClick={this.fecthPokemon('pikachu')}>Catch!</button>
+        <button className="catchButton" onClick={this.fecthPokemon('charmeleon')}>Catch!</button>
       </div>
     );
   }
 }
-
-/* .name, pokemon.id, pokemon.front_default */
